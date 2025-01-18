8c87404fd988546e06cc98104b6ebf37146b628a
AMindToThink <cs29824@sting-vm-1.cs.northwestern.edu>
2025-01-18 10:03:34 +0000

    Can evaluate mistral7b

--- src/scripts/evaluation/deep_eval.py
+++ src/scripts/evaluation/deep_eval.py
@@ -5,6 +5,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))  # Add parent directory to path
 
+from mistral7bdeepbase import get_testable_mistral
 from editing_models import InterventionModel
 from sae_lens import (
     SAE,
@@ -13,12 +14,12 @@ from sae_lens import (
 )
 import torch as t
 from transformers import AutoTokenizer, AutoModelForCausalLM
-base_name = "gemma-2-2b"
+# base_name = "gemma-2-2b"
 device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
-tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-2b")
-model = AutoModelForCausalLM.from_pretrained("google/gemma-2-2b").to(device)
+# tokenizer = AutoTokenizer.from_pretrained("google/gemma-2-2b")
+# model = AutoModelForCausalLM.from_pretrained("google/gemma-2-2b").to(device)
 benchmark = MMLU(tasks=[MMLUTask.HIGH_SCHOOL_COMPUTER_SCIENCE, MMLUTask.ASTRONOMY])
-results = benchmark.evaluate(model=model, batch_size=16)
+results = benchmark.evaluate(model=get_testable_mistral(), batch_size=5)
 print("-"*20)
 print("Overall Score: ", results)
 print("-"*20)
