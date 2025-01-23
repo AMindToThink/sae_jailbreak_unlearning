90e4b4253c92be326a2a9dfa586aec1c7606961a
AMindToThink <cs29824@sting-vm-1.cs.northwestern.edu>
2025-01-23 05:00:18 +0000

    Update lm eval

--- src/scripts/editing_models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Either leave it empty
-# or use absolute imports instead of relative ones:
-from scripts.editing_models.arena_imports import *
-from scripts.editing_models.InterventionModel import InterventionGemmaModel
