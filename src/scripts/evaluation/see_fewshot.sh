7517cc6ce646303ca251369779327efed4b48515
AMindToThink <cs29824@sting-vm-1.cs.northwestern.edu>
2025-01-25 23:10:58 +0000

    Move rmu results and make multishot evaluation script

---
+++ src/scripts/evaluation/see_fewshot.sh
@@ -0,0 +1,7 @@
+python /home/cs29824/matthew/sae_jailbreak_unlearning/src/scripts/evaluation/lm-evaluation-harness/scripts/write_out.py \
+--output_base_path ./examplefewshot_4.txt \
+--tasks mmlu_college_biology,wmdp_bio \
+--num_examples 5 \
+--num_fewshot 4 \
+--seed 42 \
+--sets test
